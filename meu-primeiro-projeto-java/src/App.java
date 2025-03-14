bec2f7e20ad776fc5d3ed8ff1ad11d598bd465b3
DiegoOmegablackMoraes <diego7cardoso@gmail.com>
2025-03-14 11:44:25 -0300

    Bora Codar!

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo!/n Bem-vindo ao meu primeiro programa em Java!");
+        System.out.println("Bora de Bootcamp Bradesco Java Cloud Native!");
+        System.out.println("Vamos codar!");
     }
 }
