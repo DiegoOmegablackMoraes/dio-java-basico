fb8bfccd620e18cb65da8d9c7217560e10678a71
DiegoOmegablackMoraes <diego7cardoso@gmail.com>
2025-03-24 19:22:24 -0300

    Anatomia das Classes 4

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,17 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        String meuNome = "Omegablack";
+        int anoNascimento = 1987;
+        boolean verdadeira  = true;
+
+        String primeiroNome = "Omega";
+        String segundoNome = "Black";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
