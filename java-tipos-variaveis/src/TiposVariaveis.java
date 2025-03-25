2b743d5c2a39c5c44ceeb38f6b88832013218cd4
DiegoOmegablackMoraes <diego7cardoso@gmail.com>
2025-03-24 21:16:56 -0300

    concluido modulo de tipos e variaveis

--- java-tipos-variaveis/src/TiposVariaveis.java
+++ java-tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "OmegaBlack";
+        double salarioMinimo = 1518.00;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 10;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14159265358979323846;
+
+        System.out.println(VALOR_DE_PI);
     }
 }
